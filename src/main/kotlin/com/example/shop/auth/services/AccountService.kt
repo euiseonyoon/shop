a0aa93af704839fa7e74953bdf0aea1182c809d3
eyoon <euiseonyoon@gmail.com>
2025-07-29 19:08:25 +0900

    feat: `AccountService.createAccount()` 구현

--- src/main/kotlin/com/example/shop/auth/services/AccountService.kt
+++ src/main/kotlin/com/example/shop/auth/services/AccountService.kt
@@ -1,16 +1,37 @@
 package com.example.shop.auth.services
 
 import com.example.shop.auth.domain.Account
+import com.example.shop.auth.domain.Authority
 import com.example.shop.auth.repositories.AccountRepository
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AccountService(
-    private val accountRepository: AccountRepository
+    private val accountRepository: AccountRepository,
+    private val passwordEncoder: PasswordEncoder,
 ) {
     @Transactional(readOnly = true)
     fun findWithAuthoritiesByEmail(email: String): Account? {
         return accountRepository.findWithAuthoritiesByEmail(email)
+    }
+
+    @Transactional
+    fun createAccount(
+        email: String,
+        rawPassword: String,
+        nickname: String?,
+        authority: Authority
+    ): Account {
+        val account = Account().apply {
+            this.username = email
+            this.password = passwordEncoder.encode(rawPassword)
+            this.enabled = true
+            this.nickname = nickname
+            addRole(authority)
+        }
+        val savedAccount = accountRepository.save(account)
+        return savedAccount
     }
 }
