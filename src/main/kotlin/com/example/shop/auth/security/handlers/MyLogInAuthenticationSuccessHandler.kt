ba3cef2f196dbee4a72a899159160d78cdb510a9
eyoon <euiseonyoon@gmail.com>
2025-07-30 21:27:06 +0900

    feat: refresh token을 HTTP Only 쿠키에 저장

--- src/main/kotlin/com/example/shop/auth/security/handlers/MyLogInAuthenticationSuccessHandler.kt
+++ src/main/kotlin/com/example/shop/auth/security/handlers/MyLogInAuthenticationSuccessHandler.kt
@@ -1,6 +1,7 @@
 package com.example.shop.auth.security.handlers
 
 import com.example.shop.auth.jwt_helpers.MyJwtTokenHelper
+import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpStatus
@@ -27,6 +28,14 @@ class MyLogInAuthenticationSuccessHandler(
         val accessToken = jwtHelper.createAccessToken(email, authentication)
         val refreshToken = jwtHelper.createRefreshToken(email)
 
+        val refreshTokenCookie = Cookie("refreshToken", refreshToken).apply {
+            isHttpOnly = true // JavaScript 접근 방지
+            path = "/" // 모든 경로에서 쿠키 사용 가능
+            maxAge = (jwtHelper.refreshTokenExpirationMs / 1000).toInt() // Refresh Token 유효 기간 (초 단위)
+            secure = true
+        }
+        response.addCookie(refreshTokenCookie)
+
         // 응답 본문에 토큰을 JSON 형식으로 작성합니다.
         response.status = HttpStatus.OK.value() // HTTP 상태 코드를 200 OK로 설정
         response.contentType = MediaType.APPLICATION_JSON_VALUE // 응답 Content-Type을 JSON으로 설정
@@ -34,8 +43,7 @@ class MyLogInAuthenticationSuccessHandler(
             """
             {
                 "message": "Login successful",
-                "accessToken": "$accessToken",
-                "refreshToken": "$refreshToken"
+                "accessToken": "$accessToken"
             }
             """.trimIndent()
         )
