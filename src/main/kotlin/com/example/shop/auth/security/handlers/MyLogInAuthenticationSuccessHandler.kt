224168dac0d42fa219f3ca3ce75849277384b3d7
eyoon <euiseonyoon@gmail.com>
2025-08-13 13:02:05 +0900

    feat: login 성공시, redis에 refresh token 저장실패하면 access token만 response body에 추가

--- src/main/kotlin/com/example/shop/auth/security/handlers/MyLogInAuthenticationSuccessHandler.kt
+++ src/main/kotlin/com/example/shop/auth/security/handlers/MyLogInAuthenticationSuccessHandler.kt
@@ -4,6 +4,7 @@ import com.example.shop.auth.jwt_helpers.MyJwtTokenHelper
 import com.example.shop.auth.models.CustomUserDetails
 import com.example.shop.auth.models.TokenResponse
 import com.example.shop.auth.utils.RefreshTokenStateHelper
+import com.example.shop.common.logger.LogSupport
 import com.example.shop.common.response.GlobalResponse
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -18,7 +19,7 @@ class MyLogInAuthenticationSuccessHandler(
     private val jwtHelper: MyJwtTokenHelper,
     private val json: Json,
     private val refreshTokenStateHelper: RefreshTokenStateHelper
-) : AuthenticationSuccessHandler {
+) : AuthenticationSuccessHandler, LogSupport() {
     override fun onAuthenticationSuccess(
         request: HttpServletRequest,
         response: HttpServletResponse,
@@ -34,14 +35,18 @@ class MyLogInAuthenticationSuccessHandler(
         val refreshToken = jwtHelper.createRefreshToken(accountId)
 
         // 새롭게 발급한 refresh token을 redis에 저장하여 상태관리
-        refreshTokenStateHelper.updateWithNewRefreshToken(accountId, refreshToken)
+        try {
+            refreshTokenStateHelper.updateWithNewRefreshToken(accountId, refreshToken)
+            // Refresh 토큰을 http only 쿠키에 저장
+            jwtHelper.setRefreshTokenOnCookie(response, refreshToken)
+        } catch (e: Exception) {
+            logger.error("refresh token not saved on redis.")
+        }
+
 
         // 응답 본문에 토큰을 JSON 형식으로 작성합니다.
         response.status = HttpStatus.OK.value() // HTTP 상태 코드를 200 OK로 설정
         response.contentType = MediaType.APPLICATION_JSON_VALUE // 응답 Content-Type을 JSON으로 설정
-
-        // Refresh 토큰을 http only 쿠키에 저장
-        jwtHelper.setRefreshTokenOnCookie(response, refreshToken)
 
         // access token이 포함된 GlobalResponse 작성
         val globalResponse = GlobalResponse.create(TokenResponse(accessToken))
