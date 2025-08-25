b8475b576956c093daf49044b61f06615072eec8
eyoon <euiseonyoon@gmail.com>
2025-08-25 15:05:10 +0900

    feat: SWAGGER_END_POINTS 추가

--- src/main/kotlin/com/example/shop/constants/UriConstants.kt
+++ src/main/kotlin/com/example/shop/constants/UriConstants.kt
@@ -10,5 +10,16 @@ const val EMAIL_PASSWORD_AUTH_URI = "${AUTH_URI_PREFIX}/login"
 const val OAUTH_AUTH_URI_PATTERN = "${AUTH_URI_PREFIX}/login/oauth/*"
 const val TOKEN_REFRESH_URI = "${AUTH_URI_PREFIX}/token/refresh"
 
-val PERMIT_ALL_END_POINTS = listOf("/", HEALTH_CHECK_URI, TOKEN_REFRESH_URI)
-val NO_API_LIMIT_END_POINTS = listOf("/", HEALTH_CHECK_URI)
+val SWAGGER_END_POINTS = listOf(
+    "/v3/api-docs/**",
+    "/swagger-ui",
+    "/swagger-ui/**",
+    "/swagger-ui.html",
+    "/swagger-resources/**",
+    "/webjars/**",
+    "/favicon**",
+)
+//val SWAGGER_END_POINTS = emptyList<String>()
+
+val PERMIT_ALL_END_POINTS = listOf("/", HEALTH_CHECK_URI, TOKEN_REFRESH_URI) + SWAGGER_END_POINTS
+val NO_API_LIMIT_END_POINTS = listOf("/", HEALTH_CHECK_URI) + SWAGGER_END_POINTS
