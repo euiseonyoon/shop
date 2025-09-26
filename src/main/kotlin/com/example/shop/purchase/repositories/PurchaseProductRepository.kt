d3d234a88caca3eea6e1fe1859d935d05f5d7908
eyoon <euiseonyoon@gmail.com>
2025-09-26 12:04:30 +0900

    feat: 필요없는 PurchaseProduct 관련 서비스, 리포지토리 삭제

--- src/main/kotlin/com/example/shop/purchase/repositories/PurchaseProductRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.shop.purchase.repositories
-
-import com.example.shop.purchase.domain.PurchaseProduct
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PurchaseProductRepository: JpaRepository<PurchaseProduct, Long> {
-
-}
