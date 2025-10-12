c89710bd485a34793d99d4e29653350b4e2a8715
eyoon <euiseonyoon@gmail.com>
2025-10-12 10:41:30 +0900

    chore: ProductStockUpdateKafkaMessage 주석 추가

--- src/main/kotlin/com/example/shop/kafka/product_stock_topic/models/ProductStockUpdateKafkaMessage.kt
+++ src/main/kotlin/com/example/shop/kafka/product_stock_topic/models/ProductStockUpdateKafkaMessage.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 data class ProductStockUpdateKafkaMessage(
     val purchaseProductId: Long?,
     val productId: Long,
-    val updateAmount: Int, // 음수 혹은 양수
+    val updateAmount: Int, // 음수 혹은 양수, 음수일때는 재고 차감, 양수일때는 재고 추가
 ) {
     init {
         require(updateAmount != 0)
