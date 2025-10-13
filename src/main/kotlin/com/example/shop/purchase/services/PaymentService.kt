ce1a279c899e30fd288de8ed9d5141306a7656bb
eyoon <euiseonyoon@gmail.com>
2025-10-14 05:55:31 +0900

    feat: PaymentService.savePayment 를 분리된 transaction에서 수행하도록 수정

--- src/main/kotlin/com/example/shop/purchase/services/PaymentService.kt
+++ src/main/kotlin/com/example/shop/purchase/services/PaymentService.kt
@@ -3,11 +3,14 @@ package com.example.shop.purchase.services
 import com.example.shop.purchase.domain.Payment
 import com.example.shop.purchase.repositories.PaymentRepository
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class PaymentService(
     private val paymentRepository: PaymentRepository
 ) {
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     fun savePayment(purchaseId: Long, paymentKey: String): Payment {
         return paymentRepository.save(Payment(purchaseId, paymentKey))
     }
